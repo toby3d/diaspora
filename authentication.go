d1d51b75cce76640f7ffd579040b56b37eb97938
Maxim Lebedev <git@toby3d.ru>
2018-01-22 19:53:51 +0500

    :building_construction: Use token as client

--- authentication.go
+++ authentication.go
@@ -2,8 +2,11 @@ package diaspora
 
 import "golang.org/x/oauth2"
 
-type Client struct{ *oauth2.Token }
+type Client struct {
+	Issuer string
+	Token  *oauth2.Token
+}
 
-func NewClient(token *oauth2.Token) *Client {
-	return &Client{token}
+func NewClient(issuer string, token *oauth2.Token) *Client {
+	return &Client{issuer, token}
 }
