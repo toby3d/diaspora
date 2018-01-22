298678cefbb9e5ae4cdfd35224010b4288c3bfd7
Maxim Lebedev <git@toby3d.ru>
2018-01-22 14:41:08 +0500

    :building_construction: Use oauth2.Token as Client only

--- request.go
+++ request.go
@@ -1,32 +1,30 @@
 package diaspora
 
 import (
+	"fmt"
+
 	log "github.com/kirillDanshin/dlog"
 	http "github.com/valyala/fasthttp"
 )
 
-func (c *Client) get(path string, args *http.Args) ([]byte, error) {
-	return c.request("GET", nil, path, args)
+func (c *Client) get(url string) ([]byte, error) {
+	return c.request(nil, url, "GET")
 }
 
-func (c *Client) post(dst []byte, path string, args *http.Args) ([]byte, error) {
-	return c.request("POST", dst, path, args)
+func (c *Client) post(dst []byte, url string) ([]byte, error) {
+	return c.request(dst, url, "POST")
 }
 
-func (c *Client) request(method string, dst []byte, path string, args *http.Args) ([]byte, error) {
-	requestURL := c.Pod
-	requestURL.Path += path
-	if args != nil {
-		requestURL.RawQuery = args.String()
-	}
-
+func (c *Client) request(dst []byte, url, method string) ([]byte, error) {
 	req := http.AcquireRequest()
 	defer http.ReleaseRequest(req)
 	req.Header.SetContentType("application/json; charset=utf-8")
 	req.Header.Set("Accept", "application/json")
 	req.Header.SetMethod(method)
-	req.Header.SetRequestURI(requestURL.String())
-	req.Header.SetHost(requestURL.Host)
+	req.Header.SetRequestURI(url)
+	if c.TokenType != "" && c.AccessToken != "" {
+		req.Header.Set("Authorization", fmt.Sprint(c.TokenType, " ", c.AccessToken))
+	}
 	req.SetBody(dst)
 
 	log.Ln("Request:")
