078d1e0e1c97fef475e6e4a14b1ce7c2300b4f67
Maxim Lebedev <git@toby3d.ru>
2018-01-22 19:54:25 +0500

    :recycle: Added support of other request methods

--- request.go
+++ request.go
@@ -1,11 +1,21 @@
 package diaspora
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 
 	log "github.com/kirillDanshin/dlog"
+	json "github.com/pquerna/ffjson/ffjson"
 	http "github.com/valyala/fasthttp"
 )
+
+type Error struct {
+	Status int    `json:"status"`
+	Error  string `json:"error"`
+}
+
+const prefix = "/api/v1"
 
 func (c *Client) get(url string) ([]byte, error) {
 	return c.request(nil, url, "GET")
@@ -15,6 +25,14 @@ func (c *Client) post(dst []byte, url string) ([]byte, error) {
 	return c.request(dst, url, "POST")
 }
 
+func (c *Client) patch(dst []byte, url string) ([]byte, error) {
+	return c.request(dst, url, "PATCH")
+}
+
+func (c *Client) delete(url string) ([]byte, error) {
+	return c.request(nil, url, "PATCH")
+}
+
 func (c *Client) request(dst []byte, url, method string) ([]byte, error) {
 	req := http.AcquireRequest()
 	defer http.ReleaseRequest(req)
@@ -22,8 +40,11 @@ func (c *Client) request(dst []byte, url, method string) ([]byte, error) {
 	req.Header.Set("Accept", "application/json")
 	req.Header.SetMethod(method)
 	req.Header.SetRequestURI(url)
-	if c.TokenType != "" && c.AccessToken != "" {
-		req.Header.Set("Authorization", fmt.Sprint(c.TokenType, " ", c.AccessToken))
+	if c.Token.TokenType != "" && c.Token.AccessToken != "" {
+		req.Header.Set(
+			"Authorization",
+			fmt.Sprint(strings.Title(c.Token.TokenType), " ", c.Token.AccessToken),
+		)
 	}
 	req.SetBody(dst)
 
@@ -35,5 +56,17 @@ func (c *Client) request(dst []byte, url, method string) ([]byte, error) {
 	err := http.Do(req, resp)
 	log.Ln("Resp:")
 	log.D(resp)
+	if err != nil {
+		return resp.Body(), err
+	}
+
+	if resp.StatusCode() < 200 && resp.StatusCode() >= 300 {
+		var data Error
+		if err = json.Unmarshal(resp.Body(), &data); err != nil {
+			return resp.Body(), err
+		}
+		return resp.Body(), errors.New(data.Error)
+	}
+
 	return resp.Body(), err
 }
